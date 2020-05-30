623e99c8e4770144b1d0b286964c3fb6d60b2f17
Ranjith Gamage <anuththaragamage104@gmail.com>
2020-05-31 01:10:33 +0530

    profile view and editing completion

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const user = express.Router();
+const db = require('../database/database');
 
 user.get('/profile',(req,res) =>{
     
-    console.log("Hi");
-    res.render('UserProfiles/UserForm');
+   const profile = db.collection('Profile');
+   let getDoc = profile.doc('1').get()
+                .then( doc => {
+                    if(!doc.exists){
+                        console.log('No such Document');
+                    } else {
+                        //console.log('Document Data :',doc.data());
+                        res.send(doc.data());
+                    }
+                })
+});
+
+user.post('/profile/edit',(req,res) => {
+        //console.log(req.body);
+        let data ={
+            Name:req.body.Name,
+            Registration_No:req.body.Registration_No,
+            Address:req.body.Address,
+            Email:req.body.Email,
+            Telephone:req.body.Telephone
+        };
+
+        let setDoc = db.collection('Profile').doc('1').set(data);
+
+        res.send('Details Updated successfully');
 });
     
 
