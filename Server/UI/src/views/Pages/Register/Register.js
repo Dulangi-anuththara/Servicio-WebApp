81ca75afcce6954c61f5aa1283f9453f9a9b60e5
Dulangi-anuththara <anuththaragamage104@gmail.com>
2020-06-29 10:56:42 +0530

    Integartion of register component

--- Server/UI/src/views/Pages/Register/Register.js
+++ Server/UI/src/views/Pages/Register/Register.js
@@ -1,68 +1,179 @@
-import React, { Component } from 'react';
-import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import React , { Component } from 'react';
+import { Button, FormControl, Select, MenuItem, InputLabel} from '@material-ui/core';
+//import background from '../assets/images/login_background.jpg';
+import fire from '../../../storage/index';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import '../Login/LoginPage.css';
+import lgo from "../../../assets/images/lgo.jpg"
+
+
 
 class Register extends Component {
-  render() {
-    return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="9" lg="7" xl="6">
-              <Card className="mx-4">
-                <CardBody className="p-4">
-                  <Form>
-                    <h1>Register</h1>
-                    <p className="text-muted">Create your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>@</InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
-                    </InputGroup>
-                    <Button color="success" block>Create Account</Button>
-                  </Form>
-                </CardBody>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-facebook mb-1" block><span>facebook</span></Button>
-                    </Col>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-twitter mb-1" block><span>twitter</span></Button>
-                    </Col>
-                  </Row>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+    state = {
+        email: '',
+        password: '',
+        full_name: '',
+        // grade: '',
+        user_type: '',
+        redirect: null
+    }
+
+    setEmail = (event) => {
+        this.setState({email: event.target.value});
+    }
+
+    setPassword = (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    setName = (event) => {
+        this.setState({full_name: event.target.value});
+    }
+
+    setUtype = (event) => {
+        this.setState({
+            user_type: event.target.value
+        }
+        );
+    }
+
+    registrationHandler = () => {
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(async (res) => {
+            var user = fire.auth().currentUser;
+            var id = user.uid
+            this.props.onAuth(id, this.state.user_type);
+
+            //update users username on firebase auth server
+            user.updateProfile({
+                displayName: this.state.full_name
+            }).then((res) => {
+                console.log(res);
+            }).catch((err => {
+                console.log(err);
+            }));
+
+            await fire.firestore().doc(`users/${id}`).set({
+                user_type: this.state.user_type,
+                full_name: this.state.full_name,
+                // grade: this.state.grade
+            }).then(res => {
+                console.log(res);
+                this.setState({redirect: <Redirect to="/dashboard"/>})
+            })
+        })
+        .catch((err) => {
+            console.log(err.message);
+            alert(err.message);
+        })
+        
+    }
+
+    render() {
+        return(
+            <div style={{
+                width: '100%',
+                height: '100vh',
+                textAlign: 'center',
+                //backgroundImage: `url(${background})`,
+                //backgroundSize: 'contain'
+                // backgroundColor: 'blue',
+                
+            }}>
+                {this.state.redirect}
+                {/* <Paper square style={{
+                    marginTop: '5%',
+                    padding: '20px',
+                    height: '80%',
+                    width: '30%',
+                    display: 'inline-block',
+
+                }}> */}
+
+
+<div id="register" className="banner h-100 ">
+  <div className="d-flex justify-content-center h-100">
+    <div className="user_card">
+      <div className="d-flex justify-content-center">
+        <div className="brand_logo_container">
+          <img src={lgo} className="brand_logo" alt="Logo" />
+        </div>
       </div>
-    );
-  }
+      <div className="d-flex justify-content-center form_container">
+        <form>
+          <div className="input-group mb-3">
+            <div className="input-group-append">
+              <span className="input-group-text"><i className="fas fa-user" /></span>
+            </div>
+            <input type="text"
+                     id="full_name"
+                     label="Full Name"
+                     onChange={this.setName} className="form-control input_user" placeholder="Full Name" />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-append">
+              <span className="input-group-text"><i className="fas fa-at" /></span>
+            </div>
+            <input type="email"
+                    id="email"
+                    label="Email"
+                    onChange={this.setEmail} className="form-control input_user" placeholder="garage@gmail.com" />
+          </div>
+          <div className="input-group mb-2">
+            <div className="input-group-append">
+              <span className="input-group-text"><i className="fas fa-key" /></span>
+            </div>
+            <input type="password" id="password"
+                    label="Password"
+                    type="password"
+                    onChange={this.setPassword} className="form-control input_pass" placeholder="password" />
+          </div>
+          <div className="form-group">
+
+            <FormControl  className="input-group-text" component="fieldset" style={{width: '100%'}} >
+            <InputLabel style={{ disableAnimation: false }} disableAnimation={false}  className="text-white">Service or Garage?  </InputLabel>
+                            <Select
+                            labelId="Grade"
+                            id="user_type"
+                            value={this.state.grade}
+                            onChange={this.setUtype}
+                            >
+                                <MenuItem value={"service"}>Service</MenuItem>
+                                <MenuItem value={"garage"}>Garage</MenuItem>
+                            </Select>
+                        </FormControl>
+                </div>
+          <div className="d-flex justify-content-center mt-3 login_container">
+          <Button onClick={this.registrationHandler} className="btn login_btn">Register</Button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-4">
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+            </div>
+        );
+    }
 }
 
-export default Register;
+
+const mapStateToProps = state => {
+    return {
+        token: state.counter
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (token, userType) => dispatch({type: 'LOGIN', value: {token: token, userType: userType}})
+    };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Register);
