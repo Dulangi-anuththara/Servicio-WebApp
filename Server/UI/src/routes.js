bb04be86573e47d6d27be4b8cedb46d1afa19a65
Dulangi-anuththara <anuththaragamage104@gmail.com>
2020-10-04 17:21:27 +0530

    merging changes

--- Server/UI/src/routes.js
+++ Server/UI/src/routes.js
@@ -45,7 +45,12 @@ const CalendarFive = React.lazy(() => import('./views/Calendar/CalendarFive'));
 const CalendarSix = React.lazy(() => import('./views/Calendar/CalendarSix'));
 const Requests = React.lazy(() => import('./views/Requests/Requests'));
 const Subscription =  React.lazy(() => import('./views/Payment/subscription'));
-const CustomerView = React.lazy(() => import('./views/Customer/CustomerView'));
+const Today = React.lazy(() => import('./views/Today/Today'))
+const InProgress = React.lazy(() => import('./views/InProgress/InProgress'));
+const ongoing = React.lazy(() => import('./views/ongoing/ongoing'));
+const Done = React.lazy(() => import('./views/Done/Done'));
+const Message = React.lazy(() => import('./views/Message/Message'));
+
 
 
 
@@ -102,19 +107,11 @@ const routes = [
   { path: '/CalendarSix', name: 'Calendar Six', component: CalendarSix },
   { path: '/Requests', name:'New Requests', component:Requests},
   { path: '/Payment', name:'New Requests', component:Subscription},
-  { path: '/CustomerView', name: 'Customer View', component: CustomerView },
   { path: '/Today', name:'Today', component:Today},
   { path: '/InProgress', name:'In Progress', component:InProgress},
   { path: '/ongoing/:id', name:'On Going', component:ongoing},
   { path: '/done', name:'Picked Up', component:Done},
-  { path: '/message', name:'Message Box', component:Message},
-
-
-
-
-
-
-  
+  { path: '/message', name:'Message Box', component:Message},  
 
 ];
 
