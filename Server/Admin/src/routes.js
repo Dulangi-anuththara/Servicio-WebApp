a1b39a83152b10f925dfb25a499dd83b4c6b05b3
Kasun Gunarathne <ktharaka56@gmail.com>
2020-10-11 13:01:07 +0530

    Edit routes

--- Server/Admin/src/routes.js
+++ Server/Admin/src/routes.js
@@ -32,6 +32,7 @@ const Badges = React.lazy(() => import('./views/Notifications/Badges'));
 const Modals = React.lazy(() => import('./views/Notifications/Modals'));
 const Colors = React.lazy(() => import('./views/Theme/Colors'));
 const Typography = React.lazy(() => import('./views/Theme/Typography'));
+const Transactions = React.lazy(() => import('./views/Theme/Transactions/Transactions'));
 const Widgets = React.lazy(() => import('./views/Widgets/Widgets'));
 const Users = React.lazy(() => import('./views/Users/Users'));
 const User = React.lazy(() => import('./views/Users/User'));
@@ -49,6 +50,7 @@ const routes = [
   { path: '/theme', exact: true, name: 'Theme', component: Colors },
   { path: '/theme/colors', name: 'Colors', component: Colors },
   { path: '/theme/typography', name: 'Typography', component: Typography },
+  { path: '/theme/transactions', name: 'Transactions', component: Transactions },
   { path: '/base', exact: true, name: 'Base', component: Cards },
   { path: '/base/cards', name: 'Cards', component: Cards },
   { path: '/base/forms', name: 'Forms', component: Forms },
