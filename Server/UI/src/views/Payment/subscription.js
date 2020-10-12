d62ef3a80295e4bd1ae46872927a628e33f1b978
Dulangi-anuththara <anuththaragamage104@gmail.com>
2020-10-12 23:54:44 +0530

    Final Touches

--- Server/UI/src/views/Payment/subscription.js
+++ Server/UI/src/views/Payment/subscription.js
@@ -76,31 +76,6 @@ class Subscription extends Component {
                   </thead>
                   <tbody>{this.renderPayments()}</tbody>
                 </Table>
-    
-                <Pagination>
-                  <PaginationItem disabled>
-                    <PaginationLink previous tag="button">
-                      Prev
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">4</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button">
-                      Next
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
               </CardBody>
             </Card>
             <Card>
